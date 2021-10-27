2da3ef7d4d053fd3fb5010b844f65628c45f673a
Nicola-Perera <nicola.perera96@mail.com>
2021-10-27 14:26:43 +0200

    created array with 5 random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ Descrizione:
     Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
     Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+//creo una lista di 5 numeri casuali
+const randomList = [];
+while (randomList.length < 5) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    if (!randomList.includes(randomNumber)) {
+        randomList.push(randomNumber);
+    }
+}
+console.log(randomList);
